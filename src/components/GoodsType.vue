b7a7abd0ca87752a70c09d46a1d2f69dd0162f1e
Tianzz05 <tianzz05@163.com>
2017-12-04 16:03:49 +0800

    v-on&&v-bind

--- src/components/GoodsType.vue
+++ src/components/GoodsType.vue
@@ -3,17 +3,20 @@
         <p>你好</p>
         <router-link :to="{name:'goodsitem'}">嵌套路由</router-link>
         <router-view></router-view>
-        <div class="">
-            <input v-bind="{inputValue}" type="text"/>
+        <div>
+            <input v-model="inputValue" type="text"/>
+            <p>{{inputValueChange}}</p>
+            <p>{{name}}</p>
+            <a @click="getGoodsTypeItems">setter</a>
         </div>
     </div>
 </template>
 <<script>
-import { Vue } from "vue";
-export default {
+import Vue from "vue";
+import router from "vue-router";
+import resource from "vue-resource";
 
-    
-    inputValue:"",
+export default {
 
   data() {
       return {
@@ -21,19 +24,36 @@ export default {
           inputValue:"",
       }
   },
+ computed: {
+     inputValueChange:{
+        get:function(value){
+            console.log(["gettter",this.inputValue]);
+            return this.inputValue.split('').reverse().join('');
+            // return this.inputValue;
+        },
+        set:function(value)  {
+            console.log(["setter",value]);
+           this.inputValue=value;
+            console.log(["setter",this.inputValue]);
+        }
+     }
+ },
+//  watch: {
+//      inputValue:function (newvalue,oldvalue) {
+//          console.log(["3-watch-oldvalue",oldvalue]);
+//          this.inputValue=newvalue;
+//          console.log(["3-watch-newValue",newvalue])
+//      }
+//  },
   mounted(){
        console.log("GoodsType");
-       this.inputValue;
-  },
-  watch: {
-      inputValue(newValue, oldValue) {
-         console.log(newValue);
-         console.log(oldValue);
-      }
+       console.log(["2-mounted",this.inputValue]);
+
+    // this.inputValueWatch;
   },
   methods: {
       getGoodsTypeItems() {
-          
+          this.inputValue="Hello Word";
       }
   },
 }
